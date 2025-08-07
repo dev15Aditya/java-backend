45dfae147280ef79e23e6cdac72ff61071568279
Aditya <aditya1115.ar@gmail.com>
2025-08-07 08:13:40 +0530

    Implemented top k elemenet of scoreboard using minHeap

--- ScoreBoard/src/ScoreBoard.java
+++ ScoreBoard/src/ScoreBoard.java
@@ -1,8 +1,6 @@
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
+import java.util.PriorityQueue;
 
 public class ScoreBoard {
     Map<Integer, Integer> scores; // _id, score
@@ -19,16 +17,29 @@ public class ScoreBoard {
         scores.put(id, scores.get(id) + score);
     }
 
-    public int topK(int k) { // O(k)
-        List<Integer> res = new ArrayList<>(scores.values());
-        if(k > res.size()){
-            k = res.size();
-        }
+    // List TC - N log (N) SC - O(N)
+    // MinHeap - N log (K) SC - O(K)
+    public int topK(int k) {
+        // List<Integer> res = new ArrayList<>(scores.values());
+        // if(k > res.size()){
+        //     k = res.size();
+        // } // O(n)
 
-        Collections.sort(res, Collections.reverseOrder());
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>((a, b) -> a-b);
+        for(int val: scores.values()){
+            minHeap.offer(val);
+            if(minHeap.size() > k) {
+                minHeap.poll();
+            }
+        } // O(Nlog(K))
+
+        // Collections.sort(res, Collections.reverseOrder()); //O(N(Log(N)))
         int sum = 0;
-        for(int i = 0; i<k; i++){
-            sum += res.get(i);
+        // for(int i = 0; i<k; i++){
+        //     sum += res.get(i);
+        // }
+        for(int v: minHeap){
+            sum += v;
         }
 
         return sum;
